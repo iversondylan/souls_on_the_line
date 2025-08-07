80a161721d67cda10857640f86450054fa996e14
Dylan Iverson <iversondylan@gmail.com>
2025-08-07 15:59:28 -0700

    started map top level generation

--- escapefrombeckford/scenes/map/map.gd
+++ escapefrombeckford/scenes/map/map.gd
@@ -1,5 +1,39 @@
-extends Control
+class_name Map extends Node2D
 
+const SCROLL_SPEED := 15
+const USABLE_ROOM = preload("res://scenes/map/usable_room.tscn")
+const MAP_LINE = preload("res://scenes/map/map_line.tscn")
 
-func _on_button_pressed() -> void:
-	Events.map_exited.emit()
+@onready var map_generator: MapGenerator = $MapGenerator
+@onready var lines: Node2D = %Lines
+@onready var rooms: Node2D = %Rooms
+@onready var visuals: Node2D = $Visuals
+@onready var camera_2d: Camera2D = $Camera2D
+
+var map_data: Array[Array]
+var n_encounters_finished: int
+var last_room: Room
+var camera_edge_x: float
+
+func _ready() -> void:
+	#camera_edge_x = MapGenerator.X_DIST * (MapGenerator.ENCOUNTERS - 1)
+	generate_new_map()
+	unlock_floor(0)
+	
+
+#do I need to make a new _input method? Maybe I can avoid scrolling
+
+func generate_new_map() -> void:
+	n_encounters_finished = 0
+	map_data = map_generator.make_map()
+	create_map()
+
+func create_map() -> void:
+	for current_encounter: Array in map_data:
+		for room: Room in current_encounter:
+			if room.next_rooms.size() > 0:
+				_spawn_room(room)
+	
+	# Boss room has no next room but we need to spawn it
+	var middle := floori(MapGenerator.MAP_HEIGHT * 0.5)
+	_spawn_room(map_data[MapGenerator.ENCOUNTERS - 1][middle])
--- escapefrombeckford/scenes/map/usable_room.gd
+++ escapefrombeckford/scenes/map/usable_room.gd
@@ -4,8 +4,56 @@ signal room_selected(room: Room)
 
 const ICONS := {
 	Room.RoomType.NOT_ASSIGNED: [null, Vector2.ONE],
-	Room.RoomType.BATTLE: 
+	Room.RoomType.BATTLE: [preload("res://assets/sprites/crossed-swords_48_w.png"), Vector2.ONE],
+	Room.RoomType.TREASURE: [preload("res://assets/sprites/chest_48_w.png"), Vector2.ONE],
+	Room.RoomType.REST: [preload("res://assets/sprites/campfire_48_w.png"), Vector2.ONE],
+	Room.RoomType.SHOP: [preload("res://assets/sprites/village_48_w.png"), Vector2.ONE],
+	Room.RoomType.BOSS: [preload("res://assets/sprites/bossskull_48_w.png"), Vector2(1.25, 1.25)]
 }
 
+@onready var sprite_2d: Sprite2D = $Visuals/Sprite2D
+@onready var line_2d: Line2D = $Visuals/Line2D
+@onready var animation_player = $AnimationPlayer
+
+var room_available := false : set = set_room_available
+var room: Room : set = set_room
+
+func _ready() -> void:
+	var test_room := Room.new()
+	test_room.type = Room.RoomType.REST
+	test_room.position = Vector2(100, 100)
+	room = test_room
+	
+	await get_tree().create_timer(1).timeout
+	room_available = true
+
+func set_room_available(new_value: bool) -> void:
+	room_available = new_value
+	
+	if room_available:
+		animation_player.play("highlight_map_icon")
+	elif !room.selected:
+		animation_player.play("RESET")
+
+func set_room(new_room: Room) -> void:
+	room = new_room
+	position = room.position
+	line_2d.rotation_degrees = randi_range(0, 360)
+	sprite_2d.texture = ICONS[room.type][0]
+	sprite_2d.scale = ICONS[room.type][1]
+
+func show_selected() -> void:
+	line_2d.modulate = Color.WHITE
+
+
+
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+	if !room_available or !event.is_action_pressed("mouse_click"):
+		return
+	
+	room.selected = true
+	animation_player.play("select_map_icon")
+
+#Called by the AnimationPlayer when the "select_map_icon" animation finishes
+func _on_usable_room_selected() -> void:
+	room_selected.emit(room)
