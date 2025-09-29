acaf2d4f0738bffc6701efa024ca5561252cb551
Dylan Iverson <iversondylan@gmail.com>
2025-09-28 20:36:23 -0700

    not much

--- escapefrombeckford/custom_resources/battle_pool.gd
+++ escapefrombeckford/custom_resources/battle_pool.gd
@@ -13,3 +13,20 @@ func _get_all_battles_for_tier(tier: int) -> Array[BattleData]:
 func _make_weight_for_tier(tier: int) -> void:
 	var battles: Array[BattleData] = _get_all_battles_for_tier(tier)
 	total_weights_by_tier[tier] = 0.0
+	
+	for battle: BattleData in battles:
+		total_weights_by_tier[tier] += battle.weight
+		battle.accumulated_weight = total_weights_by_tier[tier]
+
+func get_random_battle_for_tier(tier: int) -> BattleData:
+	var roll: float = randf_range(0.0, total_weights_by_tier[tier])
+	var battles: Array[BattleData] = _get_all_battles_for_tier(tier)
+	
+	for battle: BattleData in battles:
+		if battle.accumulated_weight > roll:
+			return battle
+	return null
+
+func init_weights() -> void:
+	for i in 3:
+		_make_weight_for_tier(i)
