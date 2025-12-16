69da60efbf084f6eb60c9d32a173465594348c8e
Dylan Iverson <iversondylan@gmail.com>
2025-12-16 08:41:41 -0800

    Update attack_action.gd

--- escapefrombeckford/cards/AttackCard/attack_action.gd
+++ escapefrombeckford/cards/AttackCard/attack_action.gd
@@ -8,7 +8,7 @@ func activate(targets: Array[Node]) -> bool:
 	var correct_targets: Array[Fighter] = correct_fighters(targets)
 	if !correct_targets:
 		return false
-		
+	
 	player.spend_mana(card_data)
 	
 	var damage_effect := DamageEffect.new()
@@ -16,7 +16,7 @@ func activate(targets: Array[Node]) -> bool:
 	damage_effect.n_damage = player.modifier_system.get_modified_value(base_damage, Modifier.Type.DMG_DEALT)
 	damage_effect.sound = card_data.sound
 	damage_effect.execute()
-
+	
 	return true
 
 func get_description(description: String, _target_fighter: Fighter = null) -> String:
