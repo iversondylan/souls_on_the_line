3def052defe6acc6dc108e7b3795155517f858fd
Dylan Iverson <iversondylan@gmail.com>
2026-02-07 16:56:09 -0800

    added overload pips

--- escapefrombeckford/cards/card_visuals.gd
+++ escapefrombeckford/cards/card_visuals.gd
@@ -15,13 +15,26 @@ class_name CardVisuals extends Control
 @onready var rarity: TextureRect = %Rarity
 @onready var card_strictly_visuals: Node2D = $CardStrictlyVisuals
 @onready var cost_label: Label = $CardStrictlyVisuals/CornerMan/CostDisplay/CostLabel
+@onready var cost_display: Node2D = %CostDisplay
 
+@export var card_angle_limit_flt: float = 180
+@export var max_card_spread_angle_flt: float = 38
+
+const OVERLOAD_PIP := preload("res://cards/overload_pip.tscn")
 
 @export var card_data: CardData : set = _set_card_data
 var cost_red: int = 0 : set = set_cost_red
 var cost_green: int = 0 : set = set_cost_green
 var cost_blue: int = 0 : set = set_cost_blue
 var _card_data_internal: CardData
+var mana_panel_radius: float
+
+func _ready() -> void:
+	mana_panel_radius = cost_container.texture.get_size().y * cost_container.scale.y * 0.5 + 2.0
+	cost_display.add_child(OVERLOAD_PIP.instantiate())
+	cost_display.add_child(OVERLOAD_PIP.instantiate())
+	cost_display.add_child(OVERLOAD_PIP.instantiate())
+	reposition_overload_pips()
 
 func _set_card_data(value: CardData) -> void:
 	assert(value != null, "CardVisuals received null CardData")
@@ -66,3 +79,34 @@ func set_cost_blue(cost: int) -> void:
 			cost_blue_sprites.frame = 0
 		1:
 			cost_blue_sprites.frame = 1
+
+func _get_overload_pips() -> Array[OverloadPip]:
+	var pips: Array[OverloadPip] = []
+	for child in cost_display.get_children():
+		if child is OverloadPip:
+			pips.push_back(child)
+	return pips
+
+func reposition_overload_pips() -> void:
+	var pips := _get_overload_pips()
+	var pip_spread_angle_flt: float = 0
+	var current_pip_angle_flt: float = 0
+	var pip_angle_increment_flt: float = 0
+
+	if pips.size() >= 2:
+		pip_spread_angle_flt = min(card_angle_limit_flt, max_card_spread_angle_flt * (pips.size() - 1))
+		current_pip_angle_flt = -pip_spread_angle_flt / 2
+		pip_angle_increment_flt = pip_spread_angle_flt / (pips.size() - 1)
+
+	for pip in pips:
+		_update_pip_transform(pip, current_pip_angle_flt)
+		current_pip_angle_flt += pip_angle_increment_flt
+
+func get_pip_position(angle_deg_flt: float) -> Vector2:
+	var x: float = mana_panel_radius * cos(deg_to_rad(angle_deg_flt + 270))
+	var y: float = mana_panel_radius * sin(deg_to_rad(angle_deg_flt + 270))
+	return Vector2(x, y)#cost_display.position + Vector2(x, y)
+
+func _update_pip_transform(pip: OverloadPip, angle_in_drag: float) -> void:
+	var pos: Vector2 = get_pip_position(angle_in_drag)
+	pip.position = pos
---
+++ escapefrombeckford/cards/overload_pip.gd
@@ -0,0 +1,1 @@
+class_name OverloadPip extends Node2D
