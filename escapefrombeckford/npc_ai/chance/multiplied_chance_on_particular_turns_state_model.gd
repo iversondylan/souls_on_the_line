ff1074229b8a417ea62e286a3d64bf6e122546f0
iversondylan <iversondylan@gmail.com>
2026-04-09 10:06:18 -0700

    quarrel spray and multiply model

--- escapefrombeckford/npc_ai/chance/multiplied_chance_on_particular_turns_state_model.gd
+++ escapefrombeckford/npc_ai/chance/multiplied_chance_on_particular_turns_state_model.gd
@@ -1,8 +1,10 @@
-# zero_chance_state_model.gd
-class_name ZeroChanceOnTurnNumbersStateModel
+# multiplied_chance_on_particular_turns_state_model.gd
+class_name MultipliedChanceOnParticularTurnsStateModel
 extends StateModel
 
+@export var chance_multiplier: float = 1.0
 @export var turn_numbers: PackedInt32Array = PackedInt32Array()
+
 
 func change_chance_weight_state_sim(ctx: NPCAIContext, action_state: Dictionary) -> void:
 	if ctx == null or action_state == null or ctx.state == null:
@@ -12,5 +14,7 @@ func change_chance_weight_state_sim(ctx: NPCAIContext, action_state: Dictionary)
 	for turn_number in turn_numbers:
 		if int(turn_number) != subjective_turn_number:
 			continue
-		action_state[Keys.CHANCE_MULT] = 0.0
+
+		var chance_mult := float(action_state.get(Keys.CHANCE_MULT, 1.0))
+		action_state[Keys.CHANCE_MULT] = chance_mult * float(chance_multiplier)
 		return
